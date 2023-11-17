8677a5a588782ab2c851e09b9afbf633a2de33fc
DefiAzzahra <150224763+DefiAzzahra@users.noreply.github.com>
2023-11-17 15:42:35 +0700

    Update page.tsx
    Add button

--- src/app/keranjang/page.tsx
+++ src/app/keranjang/page.tsx
@@ -10,11 +10,16 @@ export default function page() {
                         <div className=' w-96 p-20'>
                             <img src="assets//burger1.png" />
                         </div>
-                        <div className='pt-28 font-bold text-2xl  '>
-                            <p>Burger Beef</p>
-                            <p>Rp.25.000</p>
-                            <div className='w-24 border-red-900 bg-inherit border-solid border-[2px] rounded-[8px]'>
-                                <p>cencel</p>
+                        <div className='pt-28'>
+                            <p className='font-bold text-2xl'>Burger Beef</p>
+                            <p className='font-bold text-xl'>Rp.25.000</p>
+                            <div className='w-24 h-[40px] bg-red-900 text-slate-100 text-center border-solid border-[2px] rounded-[8px] mt-5 '>
+                                <p className='translate-y-[5px]'>cancel</p>
+                            </div>
+                        </div>
+                        <div className='pt-40'>
+                            <div className='w-44 h-[50px] bg-red-900 text-slate-100  border-solid border-[2px] rounded-[8px] mt-5'>
+                                <p className='translate-y-[10px] text-center'>shafdhg</p>
                             </div>
                         </div>
                     </div>
@@ -22,13 +27,18 @@ export default function page() {
                         <div className='mb-[370px] w-96 p-20'>
                             <img src="assets//burger1.png" />
                         </div>
-                        <div className='pt-20 font-bold text-2xl '>
-                            <p>Burger Beef</p>
-                            <p>Rp.25.000</p>
-                            <div className='w-24 border-red-900 bg-inherit border-solid border-[2px] rounded-[8px]'>
-                                <p>cencel</p>
+                        <div className='pt-20 '>
+                            <p className=' font-bold text-2xl'>Burger Beef</p>
+                            <p className='font-bold text-xl'>Rp.25.000</p>
+                            <div className='w-24 h-[40px] bg-red-900 text-slate-100 text-center border-solid border-[2px] rounded-[8px] mt-5'>
+                                <p className='translate-y-[5px]'>cancel</p>
                             </div>
 
+                        </div>
+                        <div className='pt-32'>
+                            <div className='w-44 h-[50px] bg-red-900 text-slate-100  border-solid border-[2px] rounded-[8px] mt-5'>
+                                <p className='translate-y-[10px] text-center'>shafdhg</p>
+                            </div>
                         </div>
                     </div>
                     <div className='border-t border-black w-screen h-1 mt-5 -translate-y-[400px]'>
@@ -38,7 +48,7 @@ export default function page() {
                         </div>
 
                         <div className='grid grid-cols-2'>
-                            <div className='pl-48'>
+                            <div className='pl-[180px]'>
                                 <p className='w-44 bg-red-900 text-slate-100 text-center bg-inherit border-solid border-[2px] rounded-[8px] text-xl'>View Bag</p>
                             </div>
                             <div className='pl-48'>
