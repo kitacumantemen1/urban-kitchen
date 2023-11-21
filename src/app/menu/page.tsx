7b3a10324405ae8d56fd24267833209426e444d7
DefiAzzahra <150224763+DefiAzzahra@users.noreply.github.com>
2023-11-21 16:19:50 +0700

    Update page.tsx
    Add 3d New

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import Card from '../komponen/cards'
 import Burger from '../komponen/Burger'
 import Link from 'next/link'
 import Kopi from '../komponen/Kopi'
-import Pancake from '../komponen/pancake'
+import Pancake from '../komponen/Pancake'
 
 export default function page() {
     return (
@@ -17,49 +17,51 @@ export default function page() {
             </p>
 
             <div className='grid grid-cols-2'>
-                <a href="">
-                    <div className=' w-64 h-96 ml-64 mt-20 '>
-                        <div className='bg-inherit border-solid border-[3px] border-red-900 rounded-[17px]  w-full h-64 items-center '>
-                            <Burger
-                                obj={"./assets/3d/burgerNew.gltf"}
+                <div>
+                    <a href="/menumakan">
+                        <div className=' w-64 h-96 ml-64 mt-20 '>
+                            <div className='border-solid border-[3px] border-red-900 rounded-[17px]  w-full h-64 items-center '>
+                                <div className='w-full h-[80%]'>
+                                    <Burger
+                                        obj={"./assets/3d/burgerBaru1.gltf"}
+                                    />
+                                </div>
+                                <p className='font-bold text-2xl text-center'>Foods</p>
+                            </div></div>
+                    </a>
 
-                                harga={"jjo"}
+                </div>
 
-
-
-                            />
-                            <p className='font-bold text-2xl text-center pt-4'>Foods</p>
-                        </div></div>
-                </a>
 
                 <a href="menuminum">
                     <div className=' w-64 h-96 ml-40 mt-20 '>
-                        <div className='bg-inherit border-solid border-[3px] border-red-900 rounded-[17px]  w-full h-64 items-center '>
+                        <div className='bg-inherit border-solid border-[3px] border-red-900 rounded-[17px] w-full h-64 items-center '>
 
-                            <Kopi
-                                obj={"./assets/3d/kopiNew.gltf"}
-
-                                harga={"jjo"}
-
+                            <div className='w-full h-[80%]'>
+                                <Kopi
+                                    obj={"./assets/3d/gelasBaru1.gltf"}
 
 
-                            />
-                            <p className='font-bold text-2xl text-center pt-4'>Drinks</p>
+                                />
+                            </div>
+                            <div>
+                                <p className='font-bold text-2xl text-center'>Drinks</p>
+                            </div>
+
                         </div></div></a>
 
             </div>
             <a href="menuSnacks">
                 <div className=' w-64 h-96 ml-[550px]  '>
                     <div className='bg-inherit border-solid border-[3px] border-red-900 rounded-[17px]  w-full h-64 items-center '>
-                        <Pancake
-                            obj={"./assets/3d/pancakeNew.gltf"}
-
-                            harga={"jjo"}
-
+                        <div className='w-full h-[80%]'>
+                            <Pancake
+                                obj={"./assets/3d/pancakeBaru1.gltf"}
 
 
-                        />
-                        <p className='font-bold text-2xl text-center pt-4'>Snacks</p>
+                            />
+                        </div>
+                        <p className='font-bold text-2xl text-center'>Snacks</p>
                     </div></div>
             </a>
 
