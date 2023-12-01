63cc690e27a1d29a0232eda96a9f8d5318a5e781
DefiAzzahra <150224763+DefiAzzahra@users.noreply.github.com>
2023-12-01 16:35:37 +0700

    footer
    adress end close the distanse

--- src/app/komponen/footer.tsx
+++ src/app/komponen/footer.tsx
@@ -7,17 +7,17 @@ export default function FooterSitemapLinks() {
                     <img className="h-full w-15" src="assets//logo.png" />
                 </div>
                 <div>
-                    <p className="text-red-900 font-bold md:text-4xl ms:text-2xl text-m pt-12 -translate-x-[150px]">Urban Kitchen</p>
-                    <p className="-translate-x-[150px] pt-3">Urban Kitchen is a Fast Food Restaurant bla bla bla bla bla bla bla bla bla bla bla </p>
+                    <p className="text-red-900 font-bold md:text-4xl ms:text-2xl text-m pt-12 -translate-x-[120px]">Urban Kitchen</p>
+                    <p className="-translate-x-[130px] pt-3">Urban Kitchen is a Fast Food Restaurant bla bla bla bla bla bla bla bla bla bla bla </p>
                 </div>
                 <div>
-                    <p className="font-bold text-2xl pt-12 -translate-x-[40px]">Contact Us</p>
-                    <p className="-translate-x-[40px] w-80">jl.Citra garden, Kalideres, Jakarta Barat 11840</p>
-                    <div className=' flex gap-3 py-2 -translate-x-[40px]'>
+                    <p className="font-bold text-2xl pt-12 -translate-x-[70px]">Contact Us</p>
+                    <p className="-translate-x-[70px] w-80">jl.Diponegoro no.22, citarum, kec.bandung wetan, kota bandung, jawa barat 40115</p>
+                    <div className=' flex gap-3 py-2 -translate-x-[70px]'>
                         <img src="assets//wa.png" />
                         <p className=' my-auto'>084367283982</p>
                     </div>
-                    <div className=' flex gap-3 py-2 -translate-x-[40px]'>
+                    <div className=' flex gap-3 py-2 -translate-x-[70px]'>
                         <img src="assets//pesan.png" />
                         <p className=' my-auto'>marketing@gmail.com</p>
                     </div>
